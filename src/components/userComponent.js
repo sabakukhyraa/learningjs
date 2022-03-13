d2ba8a50833f7600b12fa909b855de734889ad3f
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-03-13 20:13:17 +0300

    Progress on kodlama.io course

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -5,18 +5,6 @@ import UserService from "../services/userService.js"
 let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
 
-let user1 = new User(1, "Merve", "Metin")
-
-let user2 = new User(2, "Ali Kerem", "Ata")
-
-
-userService.add(user1)
-userService.add(user2)
-
-
-console.log(userService.listUsers())
-
-
 
 
 
